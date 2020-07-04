4b0d1112a038ef04d936caf38ff7ea5f5caeef43
DongTaeWan <bdj42@naver.com>
2020-07-04 14:05:17 +0900

    Updates

---
+++ precache-manifest.3f226cde581c279b7ede79104bb31707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7d05031c4103a2c6381c008a41f8ac",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "6106cb3f4f2c3332994a",
+    "url": "/movie/static/css/main.316baad6.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6106cb3f4f2c3332994a",
+    "url": "/movie/static/js/main.cb384d18.chunk.js"
+  },
+  {
+    "revision": "66e44caa587ffd174dd7",
+    "url": "/movie/static/js/runtime-main.3c3e1a02.js"
+  }
+]);
